36290ba569a08d0441139f1c88abba8bd5164d92
Annie080897 <anniehvs@gmail.com>
2025-06-17 16:52:42 +0400

    Create main.c

---
+++ main.c
@@ -0,0 +1,38 @@
+/*
+    PowerShell
+    1. go to directory
+        cd .\x64\Debug\ 
+
+    2. run compiled programm 
+        .\array-test.exe
+
+
+
+        ----------------------
+            x64 - processor architecture
+
+*/
+
+ //_CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main(/*int argc, char* argv[]*/) // argc - arguments count, argv - arguments value
+{
+
+    int age;
+
+    printf("Please enter your age: ");
+
+    int val = scanf("%d", &age);
+
+    printf("\nyour age is %d \n", age);
+    
+    return 0;
+}
+
